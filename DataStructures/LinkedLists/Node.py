b40b6fbc0fa3bef9b19589d6772637a788534168
Mubashir Ali <mubashir500a@gmail.com>
2022-09-28 16:53:03 +0100

    Deleted redundant Node.py module

--- DataStructures/LinkedLists/Node.py
+++
@@ -1,7 +0,0 @@
-class _Node:
-    """Nonpublic class for indivual nodes in a linked list."""
-    __slots__ = 'element', 'next'
-
-    def __init__(self, element, next):
-        self.element = element
-        self.next = next
